e9b3ade8e95805334c90e5f8d0110fc3881bacce
Nandinee <86542054+Nandoss07@users.noreply.github.com>
2021-09-17 23:00:56 +1000

    Add files via upload

---
+++ Code_4/Week_4.js
@@ -0,0 +1,13 @@
+
+var app = new Vue({
+    el: "#app", 
+data:{
+    name:"Hi I'm Nandinee Pardasani",
+    address:"Who Is Currently Studying SIT120 In Year 2021  ",
+    htmlrendering:"<h3> For This task I Will Rendar HTML Content Throught Vue </h3>",
+    htmlparagraph: "<p> This Is A HTML Paragraph Rendered Through Vue. </p>",
+    htmlimage:"", 
+}
+});
+
+
